a7bcc9f7b2637d2df41f65a8fabf5513f7a7c9e8
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-07-24 00:39:05 +0530

    Update spell-checker.py

--- spell-checker.py
+++ spell-checker.py
@@ -1,18 +1,28 @@
-from tkinter import * 
 from textblob import TextBlob
 
+from tkinter import * 
 def check():
     ori = var1.get()
     b = TextBlob(ori)
-    var2.set(b.correct())
+    correct = b.correct()
+    if var1.get() == correct:
+        stavar.set(f'Already correct word')
+    else: 
+        stavar.set(f'Correct word')
+    var2.set(correct)
+
 root=Tk()
 root.title('Spelling checker')
 root.geometry('400x200')
-Label(root,text='Spell Checker', font='Arial 20 bold').pack()
-# origi = Label(root,text='')
+root.configure(background='beige')
+Label(root,text='Spell Checker', font='Arial 20 bold').pack(pady=8)
 var1 = StringVar()
-Entry(root, textvariable=var1, font='arial 12').pack()
-Button(root,text='check', relief=SUNKEN, command=check).pack()
+stavar = StringVar()
+stavar.set('')
+Entry(root, textvariable=var1, font='arial 12').pack(pady=5)
+Button(root,text='check', relief=SUNKEN, command=check, bg='green yellow').pack(pady=5)
 var2 = StringVar()
-Entry(root, textvariable=var2, font='arial 12').pack()
+Entry(root, textvariable=var2, font='arial 12').pack(pady=5)
+sta = Entry(root,textvariable=stavar, bg='beige')
+sta.pack()
 root.mainloop()
