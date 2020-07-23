3c6a57ba45ed15c11e0c86965e28fc94dee71bfc
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-07-23 23:58:26 +0530

    Update spell-checker.py

--- spell-checker.py
+++ spell-checker.py
@@ -1,7 +1,19 @@
 from textblob import TextBlob
 
-org = 'programming langage'
-print(f'Original text = {org}')
+from tkinter import * 
+def check():
+    ori = var1.get()
+    b = TextBlob(ori)
+    var2.set(b.correct())
 
-b = TextBlob(org) 
-print(f'corrected text = {b.correct()}')
+root=Tk()
+root.title('Spelling checker')
+root.geometry('400x200')
+Label(root,text='Spell Checker', font='Arial 20 bold').pack()
+# origi = Label(root,text='')
+var1 = StringVar()
+Entry(root, textvariable=var1, font='arial 12').pack()
+Button(root,text='check', relief=SUNKEN, command=check).pack()
+var2 = StringVar()
+Entry(root, textvariable=var2, font='arial 12').pack()
+root.mainloop()
